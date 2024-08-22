812a62da86d1b1e456819c1de7082aabdc1a5b6e
ACER <ACER@LeVanSieu>
2024-08-22 08:42:48 +0700

    Le Van Sieu

---
+++ src/pheptoan/PhepToan.java
@@ -0,0 +1,23 @@
+
+package pheptoan;
+
+import java.util.Scanner;
+
+public class PhepToan {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        int f=a/b;
+        int i=a%b;
+        System.out.println("Tong 2 so a va b: "+ c);
+        System.out.println("Hieu 2 so a va b: "+ d);
+        System.out.println("Tich 2 so a va b: "+ e);
+        System.out.println("Chia 2 so a va b: "+ f);
+        System.out.println(19"Chia lay du 2 so a va b: "+ i);
+    }
+    
+}
